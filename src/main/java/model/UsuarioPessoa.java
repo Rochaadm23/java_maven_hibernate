35a92a033dec2a502605b8e30a4f3c6397e14f67
Fernando Rocha <ribeiro.ribeiro821@gmail.com>
2022-09-16 15:20:21 -0300

    Criando relacionamento entre objetos e tabelas

--- src/main/java/model/UsuarioPessoa.java
+++ src/main/java/model/UsuarioPessoa.java
@@ -1,11 +1,14 @@
 package model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 
 @Entity
 @NamedQueries({
@@ -25,6 +28,9 @@ public class UsuarioPessoa {
 	private String email;
 	private String senha;
 	private int idade;
+
+	@OneToMany(mappedBy = "usuarioPessoa")
+	private List<TelefoneUser> telefoneUsers;
 
 	public Long getId() {
 		return id;
@@ -80,6 +86,14 @@ public class UsuarioPessoa {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<TelefoneUser> getTelefoneUsers() {
+		return telefoneUsers;
+	}
+
+	public void setTelefoneUsers(List<TelefoneUser> telefoneUsers) {
+		this.telefoneUsers = telefoneUsers;
 	}
 
 	@Override
